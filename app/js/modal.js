67dbcff203a5dc5bf34eb8889a3c2468b03d4e19
oilprint <irynakozlova.k@gmail.com>
2023-07-15 17:55:13 +0300

    dropdown menu

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,29 +1,51 @@
-$(function () {
 
- 
- 
-      const button = document.querySelector('[data-modal-button]');
-      const modal = document.querySelector('[data-modal]');
-      const buttonClose = document.querySelector('[data-modal-close]');
+	/*
+  всплывающее меню
+		1. по клику на пункты верхнего меню открывать дропдаун
+		2. по клику (повторному) на эти пункты - закрывать дропдаун
+		3. по клику в любое место сайта, кроме меню - закрывать дропдаун
+	*/
 
+	const menuBtns = document.querySelectorAll('.header__link');
+	const drops = document.querySelectorAll('.dropdown');
 
-    button.addEventListener('click', function () {
-      modal.classList.remove('hidden');
-    });
+	menuBtns.forEach(el => {
+		el.addEventListener('click', (e) => {
+			let currentBtn = e.currentTarget;
+			let drop = currentBtn.closest('.header__item').querySelector('.dropdown');
 
-
-    buttonClose.addEventListener('click', function () {
-      modal.classList.add('hidden');
-    });
-
-    modal.addEventListener('click', function ( ) {
-      modal.classList.add('hidden');
-    });
+     
 
 
-    modal.querySelector('.modal').addEventListener('click', function (e) {
-      e.stopPropagation();
-    });
+			menuBtns.forEach(el => {
+				if (el !== currentBtn) {
+					el.classList.remove('header__link--active');
+				}
+			});
+        
+
+			drops.forEach(el => {
+				if (el !== drop) {
+					el.classList.remove('dropdown--active');
+				}
+			});
+
+      drop.classList.toggle('dropdown--active');
+      currentBtn.classList.toggle('header__link--active');
+		});
+	});
+
+  document.addEventListener('click', (e) => {
+		if (!e.target.closest('.header__list')) {
+			menuBtns.forEach(el => {
+				el.classList.remove('header__link--active');
+			});
+
+			drops.forEach(el => {
+				el.classList.remove('dropdown--active');
+			});
+		}
+	});
+
+
   
- 
-});
