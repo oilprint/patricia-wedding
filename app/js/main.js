ed28948bcc6907dfc0de38cf8bf306cee4ee8b8d
oilprint <irynakozlova.k@gmail.com>
2023-08-05 14:50:36 +0300

    added fixed header

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,35 @@
 console.log("test");
 
   AOS.init();
+
+  //header 
+  const wrapper = document.querySelectorAll('.wrapper');
+  const header = document.querySelector('.header');
+  const headerTop = document.querySelector('.header__top');
+
+  window.addEventListener('scroll', () => {
+    let scrollTop = window.scrollY;
+    let headerTopLength = headerTop.offsetHeight;
+    
+
+    if (scrollTop > headerTopLength) {
+
+      header.classList.add('fixed');
+      headerTop.classList.add('hidden');
+      wrapper.forEach(item => {
+        item.classList.add('scroll');
+      });
+    
+    } else {
+      header.classList.remove('fixed');
+      headerTop.classList.remove('hidden');
+       wrapper.forEach(item => {
+        item.classList.remove('scroll');
+      });
+
+    }
+  })
+  
  
 // pagination
 
