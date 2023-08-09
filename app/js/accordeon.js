81d673a8fda2c286223fdc9c8b49cb2c228e3538
oilprint <irynakozlova.k@gmail.com>
2023-08-09 13:25:46 +0300

    adaptive footer

--- app/js/accordeon.js
+++ app/js/accordeon.js
@@ -1,6 +1,6 @@
 const accordion = document.querySelectorAll('.footer__content');
 const content = document.querySelectorAll('.footer__list');
-const controlBtn = document.querySelectorAll('.footer__title');
+const controlBtn = document.querySelectorAll('.footer__btn');
 
 
 
@@ -10,72 +10,41 @@ const controlBtn = document.querySelectorAll('.footer__title');
       if (header.classList.contains('active')) {
         header.classList.remove('active');
         content[index].classList.add('hidden');
+
+        header.setAttribute('aria-expanded', false);
+        content[index].setAttribute('aria-hidden', true);
+
       } else {
         controlBtn.forEach((item, i) => {
           item.classList.remove('active');
           content[i].classList.add('hidden');
 
         });
+
         header.classList.add('active');
         content[index].classList.remove('hidden');
+        content[index].style.maxHeight = content.scrollHeight + 'px';
+
+        header.setAttribute('aria-expanded', true);
+        content[index].setAttribute('aria-hidden', false);
       }
     })
   });
 
+  
+  function updateElementClasses() {
+  const elements = document.querySelectorAll('.footer__list');
+  
+  elements.forEach(element => {
+    if (window.innerWidth <= 1024) {
+      if (!element.classList.contains('hidden')) {
+        element.classList.add('hidden');
+      }
+    } else {
+      element.classList.remove('footer__list');
+    }
+  });
+}
 
-
-      
-
-      // currentContent.classList.add('hidden');
-
-
-
-      
-
-    
-      // currentContent.classList.remove('hidden');
-
-      // content.style.maxHeight = content.scrollHeight + 'px';
-
-      // if (currentContent.classList.contains('open')) {
-      //   control.setAttribute('aria-expanded', true);
-      //   content.setAttribute('aria-hidden', false);
-      // } else {
-      //   control.setAttribute('aria-expanded', false);
-      //   content.setAttribute('aria-hidden', true);
-      //   content.style.maxHeight = null;
-      // }
-      // })
-
-
-
-
-// accordion.forEach(el => {
-//   el.addEventListener('click', (e) => {
-//     const self = e.currentTarget;
-//     const control = self.querySelector('.footer__title');
-//     const content = self.querySelector('.footer__list');
-
-
-//     accordion.forEach(item => {
-//       item.classList.add('hidden');
-//     });
-
-//     contents.forEach(item => {
-//       item.style.maxHeight = null;
-//     });
-
-//     self.classList.remove('hidden');
-
-//     content.style.maxHeight = content.scrollHeight + 'px';
-
-//     if (self.classList.contains('open')) {
-//       control.setAttribute('aria-expanded', true);
-//       content.setAttribute('aria-hidden', false);
-//     } else {
-//       control.setAttribute('aria-expanded', false);
-//       content.setAttribute('aria-hidden', true);
-//       content.style.maxHeight = null;
-//     }
-//   })
-// })
+window.onload = updateElementClasses;
+window.addEventListener('resize', updateElementClasses);
