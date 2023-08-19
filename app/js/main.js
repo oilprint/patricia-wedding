34520ab94ffa7eec7867d66360dc44ab8fcc9ff4
oilprint <irynakozlova.k@gmail.com>
2023-08-19 11:42:34 +0300

    adaptive review section

--- app/js/main.js
+++ app/js/main.js
@@ -126,6 +126,24 @@ console.log("test");
         scrollbar: {
           el: '.swiper-scrollbar',
         },
+
+         breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    576: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 640px
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
     
     });
 
