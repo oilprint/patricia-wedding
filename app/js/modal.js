ee35ca580d44cfafffe35a16e45896ec2e566907
oilprint <irynakozlova.k@gmail.com>
2023-10-03 14:08:36 +0300

    fixed modal

--- app/js/modal.js
+++ app/js/modal.js
@@ -1,52 +1,107 @@
 console.log('modal');
+
+
+
  
-  // modals
-    const modalButtons = document.querySelectorAll('[data-modal-button]');
-    const modalCloseButtons = document.querySelectorAll('[data-modal-close]');
-    const allModals = document.querySelectorAll ('[data-modal]');
-    const bodyLock = document.querySelector('body'); 
+  const modalButtons = document.querySelectorAll('[data-modal-button]');
+  const modalCloseButtons = document.querySelectorAll('[data-modal-close]');
+  const allModals = document.querySelectorAll ('[data-modal]');
+  const bodyLock = document.querySelector('body'); 
 
-    console.log(modalButtons);
-    console.log(modalCloseButtons);
-    // // Кнопки открытия модалки
-    modalButtons.forEach(function (item) {
-      item.addEventListener('click', function () {
-        console.log("clickkkk");
-
-        const modalId = this.dataset.modalButton;
-        const modal = document.querySelector('#' + modalId);
-
-        console.log(modal);
-        modal.classList.toggle('active');
-        bodyLock.classList.add('lock');
-
-        //     // Делаем так, чтобы внутри модалки коик не передавался выше родителю: находим внутри открываемой модалки блок modal-inner и запрещаем ему передавать клики наверх (его родителю)
-
-            modal.querySelector('.modal-inner').addEventListener('click', function (e) {
-              e.stopPropagation();
-            });
-      });
-    });
-
-      // // Кнопки закрытия модалки
-    modalCloseButtons.forEach(function (item) {
-      item.addEventListener('click', function () {
+  console.log(modalButtons);
+  console.log(modalCloseButtons);
     
-        const modal = this.closest('[data-modal]');
-        console.log(modal);
-  
-        modal.classList.remove('active');
-        bodyLock.classList.remove('lock');
-      });
-    });
+  modalButtons.forEach(function (item) {
+    item.addEventListener('click', function () {
+      console.log("clickkkk");
 
-// // Закрытие по клику по всему экрану
-    allModals.forEach(function (item) {
-      item.addEventListener('click', function () {
-        this.classList.remove('active');
-        bodyLock.classList.remove('lock');
+      const modalId = this.dataset.modalButton;
+      const modal = document.querySelector('#' + modalId);
+
+      console.log(modal);
+      modal.classList.remove('hidden');
+      bodyLock.classList.add('lock')
+
+      //     // Делаем так, чтобы внутри модалки коик не передавался выше родителю: находим внутри открываемой модалки блок modal-inner и запрещаем ему передавать клики наверх (его родителю)
+
+      modal.querySelector('.modal').addEventListener('click', function (e) {
+        e.stopPropagation();
       });
     });
+  });
+
+
+  modalCloseButtons.forEach(function (item) {
+    item.addEventListener('click', function () {
+  
+      const modal = this.closest('[data-modal]');
+      console.log(modal);
+
+      modal.classList.add('hidden');
+      bodyLock.classList.remove('lock');
+    });
+  });
+
+
+  allModals.forEach(function (item) {
+    item.addEventListener('click', function () {
+      this.classList.add('hidden');
+      bodyLock.classList.remove('lock');
+    });
+  }); 
+
+
+
+
+
+ 
+//   // modals
+//     const modalButtons = document.querySelectorAll('[data-modal-button]');
+//     const modalCloseButtons = document.querySelectorAll('[data-modal-close]');
+//     const allModals = document.querySelectorAll ('[data-modal]');
+//     const bodyLock = document.querySelector('body'); 
+
+//     console.log(modalButtons);
+//     console.log(modalCloseButtons);
+//     // // Кнопки открытия модалки
+//     modalButtons.forEach(function (item) {
+//       item.addEventListener('click', function () {
+//         console.log("clickkkk");
+
+//         const modalId = this.dataset.modalButton;
+//         const modal = document.querySelector('#' + modalId);
+
+//         console.log(modal);
+//         modal.classList.toggle('active');
+//         bodyLock.classList.add('lock');
+
+//         //     // Делаем так, чтобы внутри модалки коик не передавался выше родителю: находим внутри открываемой модалки блок modal-inner и запрещаем ему передавать клики наверх (его родителю)
+
+//             modal.querySelector('.modal-inner').addEventListener('click', function (e) {
+//               e.stopPropagation();
+//             });
+//       });
+//     });
+
+//       // // Кнопки закрытия модалки
+//     modalCloseButtons.forEach(function (item) {
+//       item.addEventListener('click', function () {
+    
+//         const modal = this.closest('[data-modal]');
+//         console.log(modal);
+  
+//         modal.classList.remove('active');
+//         bodyLock.classList.remove('lock');
+//       });
+//     });
+
+// // // Закрытие по клику по всему экрану
+//     allModals.forEach(function (item) {
+//       item.addEventListener('click', function () {
+//         this.classList.remove('active');
+//         bodyLock.classList.remove('lock');
+//       });
+//     });
 
 
 
